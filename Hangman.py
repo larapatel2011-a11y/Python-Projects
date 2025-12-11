39f35035277657506bc1e5470e21ab45f11250ea
larapatel2011-a11y <larapatel2011@gmail.com>
2025-12-11 17:20:32 +0000

    Add files via upload

--- Hangman.py
+++ Hangman.py
@@ -1,102 +1,114 @@
 import random
 words = ["table", "house", "mirror", "brush"]
-keyword = random.choice (words)
-len(keyword)
-underscores = ["_"]*len (keyword)
+score = 0
+def game():
+  global score
+  keyword = random.choice (words)
+  len(keyword)
+  underscores = ["_"]*len (keyword)
 
 
-hangman_stages = [
-'''
-    |
-    |
-    |
-    |
-    |
-''',
-'''
----------
-|
-|
-|
-|
-|
-''',
-'''
-    ---------
-      |.   |
-      |    
-      |   
-      |   
+  hangman_stages = [
+  '''
       |
-      ''',
-      '''
-    ---------
-      |.   |
-      |    O
-      |   
-      |   
       |
-      ''',
-      '''
-    ---------
-      |.   |
-      |    O
-      |    |
-      |   
       |
-      ''',
-'''
-    ---------
-      |.   |
-      |    O
-      |   \\|
-      |   
       |
-      ''',
-      '''
-    ---------
-      |.   |
-      |    O
-      |   \\|/
-      |   
       |
-      ''',
-      '''
-    ---------
-      |.   |
-      |    O
-      |   \\|/
-      |   / 
-      |
-      ''',
-      '''
-    ---------
-      |.   |
-      |    O
-      |   \\|/
-      |   / \\
-      |
-      '''
+  ''',
+  '''
+  ---------
+  |
+  |
+  |
+  |
+  |
+  ''',
+  '''
+      ---------
+        |.   |
+        |    
+        |   
+        |   
+        |
+        ''',
+        '''
+      ---------
+        |.   |
+        |    O
+        |   
+        |   
+        |
+        ''',
+        '''
+      ---------
+        |.   |
+        |    O
+        |    |
+        |   
+        |
+        ''',
+  '''
+      ---------
+        |.   |
+        |    O
+        |   \\|
+        |   
+        |
+        ''',
+        '''
+      ---------
+        |.   |
+        |    O
+        |   \\|/
+        |   
+        |
+        ''',
+        '''
+      ---------
+        |.   |
+        |    O
+        |   \\|/
+        |   / 
+        |
+        ''',
+        '''
+      ---------
+        |.   |
+        |    O
+        |   \\|/
+        |   / \\
+        |
+        '''
 
-]
+  ]
 
-counter = -1
+  counter = -1
+
+  while True:
+      print (" ".join(underscores))
+
+      letter = input("Guess a letter: ")
+
+      if letter in keyword:
+          for i in range(len(keyword)):
+              if letter == keyword [i]:
+                  underscores[i] = letter
+      else:
+          counter = counter + 1
+          if counter == 9:
+              print("You have no more guesses")
+              break
+          print (hangman_stages [counter])
+      if "_" not in underscores:
+          print("You have guessed the word")
+          score = score + 1
+          break
 
 while True:
-    print (" ".join(underscores))
-
-    letter = input("Guess a letter: ")
-
-    if letter in keyword:
-        for i in range(len(keyword)):
-            if letter == keyword [i]:
-                underscores[i] = letter
-    else:
-        counter = counter + 1
-        if counter == 9:
-            print("You have no more guesses")
-            break
-        print (hangman_stages [counter])
-    if "_" not in underscores:
-        print("You have guessed the word")
-        break
+  again = input("Do you want to play again?")
+  if again == "yes":
+    game()
+  else:
+    print ("Game over. The score is" , score)
+    break
