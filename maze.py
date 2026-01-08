3d909b5e2f84f2db0cd55205229dd5479a444f6c
larapatel2011-a11y <larapatel2011@gmail.com>
2026-01-08 17:43:46 +0000

    Add files via upload

--- maze.py
+++ maze.py
@@ -1,7 +1,11 @@
 import turtle
+import time
 t= turtle.Turtle()
 s= turtle.Screen()
 t.shape("turtle")
+
+t.penup()
+t.goto(-264,264)
 
 maze=[
     "XXXXXXXXXXXXXXX",
@@ -12,14 +16,16 @@ maze=[
     "XX XX XXX    XX",
     "XX  X XXXXXX  X",
     "XXX X   XXXXX X",
-    "XXX XXX XXXXX X",
-    "X             X",
-    "X             X",
-    "X             X",
-    "X             X",
-    "X             X",
+    "XXX XXX XXXXXFX",
+    "XXX X   XXXXX X",
+    "XXX XXXXXXXX  X",
+    "XXX        XXXX",
+    "XXXXXXXXXX    X",
+    "XXXXXXXXXXX XXX",
     "XXXXXXXXXXXXXXX"
 ]
+
+walls = []
 
 for y in range (len(maze)):
     for x in range (len(maze[0])):
@@ -27,24 +33,56 @@ for y in range (len(maze)):
         turtley = 288-(y*24)
         if maze[y][x] == "X":
             wall= turtle.Turtle()
+            walls.append(wall)
             wall.shape("square")
             wall.color("blue")
             wall.penup()
             wall.speed(100)
             wall.goto(turtlex,turtley)
+        if maze[y][x] == "F":
+            food = turtle.Turtle()
+            food.shape("circle")
+            food.color("red")
+            food.penup()
+            food.goto(turtlex,turtley)
 
+def check(x,y):
+    for wall in walls:
+       if  wall.xcor() == x and wall.ycor() == y :
+           return False
+    return True
 
 def right():
     xposition = t.xcor()+24
-    t.setx (xposition)
+    if check(xposition, t.ycor()):
+        t.setx (xposition)
+
+def left():
+    xposition = t.xcor()-24
+    if check(xposition, t.ycor()):
+        t.setx (xposition)
+
+def up():
+    yposition = t.ycor()+24
+    if check(t.xcor(), yposition):
+        t.sety (yposition)
+
+def down():
+    yposition = t.ycor()-24
+    if check(t.xcor(), yposition):
+        t.sety (yposition)
 
 s.listen()
 s.onkey(right,"Right")
+s.onkey(left, "Left")
+s.onkey(up, "Up" )
+s.onkey(down, "Down")
 
 
-
-
-
+while True:
+    if t.distance(food)==0:
+        t.write("Congratulations, you found the food")
+    time.sleep(1)
 
 
 
